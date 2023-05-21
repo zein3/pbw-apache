3cd68efdebbfb3828ab893f905beed8da36a51ab
zein3 <zeinhaddad02@gmail.com>
2023-05-21 18:27:34 +0700

    praktikum 11 done

--- sesi11/php11F_suggestion.js
+++ sesi11/php11F_suggestion.js
@@ -14,12 +14,14 @@ function showHint(str) {
     if (xhttp.readyState != 4) {
       return;
     } else if (xhttp.status == 200) {
-      txtHint.innerText = xhttp.responseText;
+      // txtHint.innerText = xhttp.responseText;
+      let names = JSON.parse(xhttp.responseText).map(i => i.name);
+      txtHint.innerText = (names.length > 0) ? names.join(", ") : "no suggestion";
     } else {
       alert("Something went wrong.");
     }
   }
 
-  xhttp.open("GET", "php11F_gethint.php?keyword=" + str, true);
+  xhttp.open("GET", "php11F_gethint2.php?keyword=" + str, true);
   xhttp.send();
 }
